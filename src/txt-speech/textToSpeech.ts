9c54924fbc76f491040acf7ab7686d1c459a319b
Philip Grabenhorst <phlip007@gmail.com>
2023-04-08 16:26:16 -0700

    Remove default voice option

--- src/txt-speech/textToSpeech.ts
+++ src/txt-speech/textToSpeech.ts
@@ -2,28 +2,15 @@
 
 
 const PITCH_SYNTH = 1;
-const RATE_SYNTH = 1;
-
-
-function getVoice() {
-    let synth = window.speechSynthesis;
-    let voices = synth.getVoices();
-    let ua = navigator.userAgent.toLowerCase()
-    if (ua.includes("safari") && !ua.includes("chrome"))
-        return
-    return voices.find(v => v.lang == "en-GB") as SpeechSynthesisVoice;
-}
+const RATE_SYNTH = 0.9;
 
 export function speak(text:string) {
     let synth = window.speechSynthesis;
-    let synthVoice = getVoice();    
     synth.cancel();
     return new Promise((res, rej) => {
         console.log(`Speaking: ${text}`);
         // create the utterance
         let utterance = new SpeechSynthesisUtterance(text);
-        if (synthVoice)
-            utterance.voice = synthVoice;
         utterance.pitch = PITCH_SYNTH;
         utterance.rate = RATE_SYNTH;
         // speak it
